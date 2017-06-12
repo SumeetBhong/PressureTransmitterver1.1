55d862c39f9e8b8c11b7be37ef6f8815e4121ce9
SumeetBhong <sumeet.bhong@gmail.com>
2017-06-12 12:12:03 +0530

    My commit message

---
+++ app/src/main/java/com/orion_instruments/www/pressuretransmitterver11/Datalogger.java
@@ -0,0 +1,14 @@
+package com.orion_instruments.www.pressuretransmitterver11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Datalogger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datalogger);
+        setTitle("Datalogger");
+    }
+}
---
+++ app/src/main/java/com/orion_instruments/www/pressuretransmitterver11/DeviceSettings.java
@@ -0,0 +1,14 @@
+package com.orion_instruments.www.pressuretransmitterver11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_settings);
+        setTitle("Device Settings");
+    }
+}
